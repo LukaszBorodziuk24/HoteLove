ed5838d92d48e8eb5c1bbb09169b5455bee6d4b0
tacosR1 <lukaszborodziuk24@gmail.com>
2023-04-25 09:55:05 +0200

    Add project files.

---
+++ HoteLove/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoteLove.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
